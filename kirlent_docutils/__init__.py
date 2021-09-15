81d0da27461bf2810cd2ec326b1c0eb6fdafcaf0
H. Turgut Uyar <uyar@tekir.org>
2021-09-15 17:38:24 +0300

    initialize project

---
+++ kirlent_docutils/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2020-2021 H. Turgut Uyar <uyar@tekir.org>
+#
+# kirlent_docutils is released under the BSD license.
+# Read the included LICENSE.txt file for details.
+
+"""Custom writers for docutils."""
+
+__version__ = "0.1.0"
