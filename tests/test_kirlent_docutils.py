85a4d6025f580743d7b1285c061a9bd4fb84b6f9
H. Turgut Uyar <uyar@tekir.org>
2023-02-01 15:44:05 +0300

    drop support for py37

--- tests/test_kirlent_docutils.py
+++ tests/test_kirlent_docutils.py
@@ -1,7 +1,7 @@
-from pkg_resources import get_distribution
+from importlib import metadata
 
 import kirlent_docutils
 
 
 def test_installed_version_should_match_tested_version():
-    assert get_distribution("kirlent_docutils").version == kirlent_docutils.__version__
+    assert metadata.version("kirlent_docutils") == kirlent_docutils.__version__
