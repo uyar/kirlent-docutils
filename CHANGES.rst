e5e056e071de0a9c0b395e73773282a2aeaafaed
H. Turgut Uyar <uyar@tekir.org>
2023-03-01 21:43:30 +0300

    prepare version 0.3

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,4 +1,4 @@
-0.3 (unreleased)
+0.3 (2023-03-01)
 ----------------
 
 - Remove properties and custom stylesheets.
--- kirlent_docutils/__init__.py
+++ kirlent_docutils/__init__.py
@@ -5,7 +5,7 @@
 
 """Custom writers for docutils."""
 
-__version__ = "0.2"
+__version__ = "0.3"
 
 from docutils.core import publish_cmdline
 
